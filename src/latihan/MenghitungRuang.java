b3f1a9eab328685012a3a7b369e649655d98e74f
Muhammad Abdanul Ikhlas <121916529+AbdanulIkhlas@users.noreply.github.com>
2023-03-25 06:41:33 +0700

    Add files via upload

---
+++ src/latihan/MenghitungRuang.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package latihan;
+
+/**
+ *
+ * @author LENOVO
+ */
+public interface MenghitungRuang {
+    void HitungLuasPermukaan();
+    void HitungVolume();
+}
---
+++ src/latihan/PersegiPanjang.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PersegiPanjang implements MenghitungBidang{
+    private double panjang,lebar,luas,keliling;
+
+    public PersegiPanjang(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    
+    // override
+    @Override
+    public void HitungLuas() {
+        luas=panjang*lebar;
+    }
+
+    @Override
+    public void HitungKeliling() {
+        keliling=2*(panjang+lebar);
+    }
+
+    // encapsulation
+    public double getLuas() {
+        return luas;
+    }
+
+    public double getKeliling() {
+        return keliling;
+    }
+ 
+    public double getPanjang() {
+        return panjang;
+    }
+
+    public double getLebar() {
+        return lebar;
+    }
+}
+
+
+
